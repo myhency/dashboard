bf80b2f13526e827a9b1675da5782d66d75f6f7c
james <hency.yeo@gmail.com>
2019-05-02 21:38:07 +0900

    james test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-table/react-table.css';
+import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.bubble.css';
+import 'assets/css/style.scss';
+import ScrollToTop from 'utils/ScrollToTop';
+import AuthLayout from 'layouts/auth/AuthLayout';
+import MainLayout from 'layouts/main/MainLayout';
+import NotFound from 'views/common/NotFound';
+import PrivateRoute from 'components/PrivateRoute';
+import { signIn } from 'store/modules/auth';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
